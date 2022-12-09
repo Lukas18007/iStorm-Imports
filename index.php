9a22fa16eda8d497b8165c596c7e5e9dcc547235
Lukas Leandro <leandrolukas916@gmail.com>
2022-12-08 23:35:35 -0300

    first commit - instalando requisitos

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bem-vindo! | iStorm</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <script src="./jquery/jquery-3.6.1.js"></script>
+</head>
+<body>
+
+    <button class="btn btn-danger" id="teste">teste</button>
+    
+    <script>
+        $("#teste").on("click", function(){
+            alert("deu certo");
+        });
+    </script>
+    <script src="./js/bootstrap.bundle.js"></script>
+</body>
+</html>
